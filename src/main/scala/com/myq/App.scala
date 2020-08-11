3220e38289122b1bf57e399db2d8c726f08fb9c8
racris <625404504@qq.com>
2020-08-11 22:25:36 +0800

    spark各种简单模型--首次提交

---
+++ src/main/scala/com/myq/App.scala
@@ -0,0 +1,9 @@
+package com.myq
+
+/**
+ * Hello world!
+ *
+ */
+/*object App extends Application {
+  println( "Hello World!" )
+}*/
---
+++ src/test/scala/com/myq/AppTest.scala
@@ -0,0 +1,17 @@
+package com.myq
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
